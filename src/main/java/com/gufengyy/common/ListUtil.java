6d8ca9315d809f67a2f9ee02657c7cbb13803da1
zhanglei <hzl0915@126.com>
2016-02-25 18:02:51 +0800

    完成列表中String类型与其它Number类型数据的相互转化

--- src/main/java/com/gufengyy/common/ListUtil.java
+++ src/main/java/com/gufengyy/common/ListUtil.java
@@ -8,7 +8,7 @@ import java.util.stream.Collectors;
  * 有关列表处理的工具类。
  *
  * @author zhanglei
- * @date 2016年2月25日 下午4:40:12 
+ * @date 2016年2月25日 下午4:40:12
  *
  */
 public abstract class ListUtil {
@@ -25,4 +25,169 @@ public abstract class ListUtil {
                 .collect(Collectors.toList());
     }
 
+    /**
+     * 将Number类型的列表转化为String类型的列表.
+     * 
+     * @param input
+     * @return
+     */
+    public static List<String> number2String(List<? extends Number> input) {
+        return operator(input, x -> String.valueOf(x));
+    }
+
+    /**
+     * 将String类型的列表转化为Integer类型的列表.
+     * 
+     * @param input
+     * @return
+     * 
+     * @exception NumberFormatException
+     *                - if the string does not contain a parsable integer
+     */
+    public static List<Integer> string2Int(List<String> input) {
+        return operator(input, x -> Integer.parseInt(x));
+    }
+
+    /**
+     * 将String类型的列表转化为Long类型的列表.
+     * 
+     * @param input
+     * @return
+     * 
+     * @exception NumberFormatException
+     *                - if the string does not contain a parsable long
+     */
+    public static List<Long> string2Long(List<String> input) {
+        return operator(input, x -> Long.parseLong(x));
+    }
+
+    public static List<? extends Number> string2NumberQuietly(
+            List<String> input, Function<String, ? extends Number> function) {
+        return operator(input, x -> {
+            try {
+                return function.apply(x);
+            } catch (Exception ignore) {
+                return null;
+            }
+        }).stream().filter(x -> {
+            if (x != null) {
+                return true;
+            } else {
+                return false;
+            }
+        }).collect(Collectors.toList());
+    }
+
+    /**
+     * 将String类型的列表转化为Integer类型的列表。不抛出异常，直接将非Integer类型的元素舍弃。
+     * 
+     * @param input
+     * @return
+     */
+    @SuppressWarnings("unchecked")
+    public static List<Integer> string2IntQuietly(List<String> input) {
+        return (List<Integer>) string2NumberQuietly(input,
+                x -> Integer.parseInt(x));
+    }
+
+    /**
+     * 将String类型的列表转化为Long类型的列表。不抛出异常，直接将非Long类型的元素舍弃。
+     * 
+     * @param input
+     * @return
+     */
+    @SuppressWarnings("unchecked")
+    public static List<Long> string2LongQuietly(List<String> input) {
+        return (List<Long>) string2NumberQuietly(input, x -> Long.parseLong(x));
+    }
+
+    /**
+     * 将String类型的列表转化为Float类型的列表。不抛出异常，直接将非Float类型的元素舍弃。
+     * 
+     * @param input
+     * @return
+     */
+    @SuppressWarnings("unchecked")
+    public static List<Float> string2FloatQuietly(List<String> input) {
+        return (List<Float>) string2NumberQuietly(input,
+                x -> Float.parseFloat(x));
+    }
+
+    /**
+     * 将String类型的列表转化为Double类型的列表。不抛出异常，直接将非Double类型的元素舍弃。
+     * 
+     * @param input
+     * @return
+     */
+    @SuppressWarnings("unchecked")
+    public static List<Double> string2DoubleQuietly(List<String> input) {
+        return (List<Double>) string2NumberQuietly(input,
+                x -> Double.parseDouble(x));
+    }
+
+    public static List<? extends Number> string2NumberIngoreException(
+            List<String> input, Function<String, ? extends Number> function,
+            Number defaultValue) {
+        return operator(input, x -> {
+            try {
+                return function.apply(x);
+            } catch (Exception ignore) {
+                return defaultValue;
+            }
+        });
+    }
+
+    /**
+     * 将String类型的列表转化为Integer类型的列表。不抛出异常，将非Integer类型的元素由defaultVaule代替。
+     * 
+     * @param input
+     * @param defaultValue
+     * @return
+     */
+    @SuppressWarnings("unchecked")
+    public static List<Integer> string2Int(List<String> input,
+            Integer defaultValue) {
+        return (List<Integer>) string2NumberIngoreException(input,
+                x -> Integer.parseInt(x), defaultValue);
+    }
+
+    /**
+     * 将String类型的列表转化为Long类型的列表。不抛出异常，将非Long类型的元素由defaultVaule代替。
+     * 
+     * @param input
+     * @param defaultValue
+     * @return
+     */
+    @SuppressWarnings("unchecked")
+    public static List<Long> string2Long(List<String> input, Long defaultValue) {
+        return (List<Long>) string2NumberIngoreException(input,
+                x -> Long.parseLong(x), defaultValue);
+    }
+
+    /**
+     * 将String类型的列表转化为Double类型的列表。不抛出异常，将非Long类型的元素由Double代替。
+     * 
+     * @param input
+     * @param defaultValue
+     * @return
+     */
+    @SuppressWarnings("unchecked")
+    public static List<Float> string2Float(List<String> input, Float defaultValue) {
+        return (List<Float>) string2NumberIngoreException(input,
+                x -> Float.parseFloat(x), defaultValue);
+    }
+
+    /**
+     * 将String类型的列表转化为Double类型的列表。不抛出异常，将非Long类型的元素由Double代替。
+     * 
+     * @param input
+     * @param defaultValue
+     * @return
+     */
+    @SuppressWarnings("unchecked")
+    public static List<Double> string2Double(List<String> input,
+            Double defaultValue) {
+        return (List<Double>) string2NumberIngoreException(input,
+                x -> Double.parseDouble(x), defaultValue);
+    }
 }
--- src/test/java/com/gufengyy/common/ListUtilTest.java
+++ src/test/java/com/gufengyy/common/ListUtilTest.java
@@ -8,12 +8,95 @@ import junit.framework.TestCase;
 
 public class ListUtilTest extends TestCase {
 
-    public void testOperator(){
-        List<Integer> list = Lists.newArrayList(1,2,3,4,5);
-        List<String> result = ListUtil.operator(list, x -> x + "");
-        for(int i=0;i<result.size();i++){
+    public void testOperator() {
+        List<Integer> list = Lists.newArrayList(1, 2, 3, 4, 5);
+        List<String> result = ListUtil.operator(list, x -> String.valueOf(x));
+        for (int i = 0; i < result.size(); i++) {
             assertTrue(result.get(i) instanceof String);
             assertEquals(String.valueOf(list.get(i)), result.get(i));
         }
+
+        List<Double> doubleList = Lists.newArrayList(1.0d, 2.1d, 3.3d, 4.5d,
+                5.6d);
+        List<String> douleResult = ListUtil.operator(doubleList,
+                x -> String.valueOf(x));
+        for (int i = 0; i < result.size(); i++) {
+            assertTrue(douleResult.get(i) instanceof String);
+            assertEquals(String.valueOf(doubleList.get(i)), douleResult.get(i));
+        }
     }
+
+    public void testInt2String() {
+        List<Integer> input = Lists.newArrayList(1, 2, 3, 4, 5);
+        List<String> result = ListUtil.number2String(input);
+        for (int i = 0; i < result.size(); i++) {
+            assertTrue(result.get(i) instanceof String);
+            assertEquals(String.valueOf(input.get(i)), result.get(i));
+        }
+    }
+
+    public void testString2IntQuietly() {
+        List<String> input = Lists.newArrayList("1", "A", null, "0", "-3", "a",
+                "5b", String.valueOf(Integer.MAX_VALUE),
+                String.valueOf(Integer.MIN_VALUE));
+        List<Integer> result = ListUtil.string2IntQuietly(input);
+        System.out.println(result);
+    }
+    
+    public void testString2LongQuietly() {
+        List<String> input = Lists.newArrayList("1", "A", null, "0", "-3", "a",
+                "5b", String.valueOf(Long.MAX_VALUE),
+                String.valueOf(Long.MIN_VALUE));
+        List<Long> result = ListUtil.string2LongQuietly(input);
+        System.out.println(result);
+    }
+    
+    public void testString2DoubleQuietly() {
+        List<String> input = Lists.newArrayList("1.333333333d", "A", null, "0", "-3.68", "a",
+                "5b", String.valueOf(Double.MAX_VALUE),
+                String.valueOf(Double.MIN_VALUE));
+        List<Double> result = ListUtil.string2DoubleQuietly(input);
+        System.out.println(result);
+    }
+    
+    public void testString2FloatQuietly() {
+        List<String> input = Lists.newArrayList("1.3333333f", "A", null, "0", "-3.68", "a",
+                "5b", String.valueOf(Float.MAX_VALUE),
+                String.valueOf(Float.MIN_VALUE));
+        List<Float> result = ListUtil.string2FloatQuietly(input);
+        System.out.println(result);
+    }
+    
+    public void testString2IntDefaultValue() {
+        List<String> input = Lists.newArrayList("1", "A", null, "0", "-3", "a", "3.0",
+                "5b", String.valueOf(Integer.MAX_VALUE),
+                String.valueOf(Integer.MIN_VALUE));
+        List<Integer> result = ListUtil.string2Int(input, 10000);
+        System.out.println(result);
+    }
+    
+    public void testString2LongDefaultValue() {
+        List<String> input = Lists.newArrayList("1", "A", null, "0", "-3", "a", "3.0",
+                "5b", String.valueOf(Integer.MAX_VALUE),
+                String.valueOf(Integer.MIN_VALUE));
+        List<Long> result = ListUtil.string2Long(input, 10000L);
+        System.out.println(result);
+    }
+    
+    public void testString2DoubleDefaultValue() {
+        List<String> input = Lists.newArrayList("1.333333333d", "A", null, "0", "-3.68", "a",
+                "5b", String.valueOf(Double.MAX_VALUE),
+                String.valueOf(Double.MIN_VALUE));
+        List<Double> result = ListUtil.string2Double(input, 9.9999d);
+        System.out.println(result);
+    }
+    
+    public void testString2FloatDefaultValue() {
+        List<String> input = Lists.newArrayList("1.3333333f", "A", null, "0", "-3.68", "a",
+                "5b", String.valueOf(Float.MAX_VALUE),
+                String.valueOf(Float.MIN_VALUE));
+        List<Float> result = ListUtil.string2Float(input,9.999f);
+        System.out.println(result);
+    }
+
 }
